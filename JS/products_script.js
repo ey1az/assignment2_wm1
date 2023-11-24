bf5791dc9c1848cbb2eb2a204bfbad1a5c0304af
ey1az <aisser.cool@gmail.com>
2023-11-24 16:51:07 +0400

    Products Page (Complete) - Commit 5

--- JS/products_script.js
+++ JS/products_script.js
@@ -6,16 +6,22 @@ const itemsPerPage = 10;
 
 const productListElement = document.getElementById('list-of-products');
 const paginationElement = document.getElementById('pagination');
+const searchInput = document.getElementById('search-input');
+const categorySelect = document.getElementById('category-select');
 
 function initializePage() {
     fetchData()
         .then(data => {
             productsGiven = data;
-            displayProductList(productsGiven);
+            updateCategoryOptions();
+            displayProductList(1, productsGiven);
         })
         .catch(error => {
             productListElement.innerHTML = 'Fetching data error!';
         });
+
+    searchInput.addEventListener('input', searchHandler);
+    categorySelect.addEventListener('change', categoryHandler);
 
     document.addEventListener('click', (event) => {
         const target = event.target;
@@ -40,21 +46,22 @@ async function fetchData() {
     }
 }
 
-function displayProductList(filteredProducts) {
+function displayProductList(page, filteredProducts) {
     const totalProducts = filteredProducts.length;
     const totalPages = Math.ceil(totalProducts / itemsPerPage);
+    currentPage = page;
 
     const begin = (page - 1) * itemsPerPage;
     const end = begin + itemsPerPage;
 
     const paginProducts = filteredProducts.slice(begin, end);
-    
+
     productListElement.innerHTML = '';
     paginProducts.forEach(product => {
         const prodHTML = `
             <div class="card">
                 <h1 class="title">${product.title}</h1>
-                <p class="price">$${product.price.toFixed(2)}</p>
+                <p class="price">$${product.price}</p>
                 <p class="discount">${product.discountPercentage}% off</p>
                 <p class="category">${product.category}</p>
                 <p class="stock">Stock: ${product.stock}</p>
@@ -65,6 +72,7 @@ function displayProductList(filteredProducts) {
 
         productListElement.innerHTML += prodHTML;
     });
+
     displayPagination(totalPages);
 }
 
@@ -72,7 +80,57 @@ function displayPagination(totalPages) {
     paginationElement.innerHTML = '';
     for (let i = 1; i <= totalPages; i++) {
         const button = document.createElement('button');
+        button.textContent = i;
         button.classList.add('pagination-button');
-        button.addEventListener('click', () => displayProductList(i));
+        button.addEventListener('click', () => displayProductList(i, getFilteredProducts()));
+        if (i === currentPage) {
+            button.classList.add('active');
+        }
+        paginationElement.appendChild(button);
     }
 }
+
+function getFilteredProducts() {
+    const searchForProduct = searchInput.value.trim().toLowerCase();
+    const selectedCategory = categorySelect.value.toLowerCase();
+
+    return productsGiven.filter(product => {
+        const matchTitle = product.title.toLowerCase().includes(searchForProduct);
+        const matchDescription = product.description.toLowerCase().includes(searchForProduct);
+        const matchCategory = selectedCategory === 'all' || product.category.toLowerCase() === selectedCategory;
+
+        return (matchTitle || matchDescription) && matchCategory;
+    });
+}
+
+function searchHandler() {
+    displayProductList(1, getFilteredProducts());
+}
+
+function categoryHandler() {
+    displayProductList(1, getFilteredProducts());
+}
+
+function updateCategoryOptions() {
+    const categories = Array.from(new Set(productsGiven.map(product => product.category)));
+
+    categorySelect.innerHTML = '';
+    const categoriesOptionAll = document.createElement('option');
+    categoriesOptionAll.value = 'all';
+    categoriesOptionAll.textContent = 'All Categories';
+    categorySelect.appendChild(categoriesOptionAll);
+
+    categories.forEach(category => {
+        const option = document.createElement('option');
+        option.value = category.toLowerCase();
+        option.textContent = category;
+        categorySelect.appendChild(option);
+    });
+}
+
+function showProductDetails(productId) {
+    window.location.href = `products_details.html?id=${productId}`;
+    searchInput.value = '';
+    categorySelect.value = 'all';
+}
+
